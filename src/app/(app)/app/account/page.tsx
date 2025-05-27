871b3ee8ac433f893d0472cbb0536d4dfab6b430
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-05-27 17:54:10 +0100

    Style update to account page

--- src/app/(app)/app/account/page.tsx
+++ src/app/(app)/app/account/page.tsx
@@ -1,3 +1,14 @@
+import ContentBlock from '@/components/content-block';
+import H1 from '@/components/h1';
+
 export default function AccountPage() {
-  return <div>AccountPage</div>;
+  return (
+    <main>
+      <H1 className='my-8 text-white'>Your account</H1>
+
+      <ContentBlock className='h-[32rem] flex items-center justify-center'>
+        <p>logged in </p>
+      </ContentBlock>
+    </main>
+  );
 }
