4361a7cdcb985fd4de527c560d3f6aff772dd6d7
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-05-27 12:41:39 +0100

    Get data for pets list

--- src/app/(app)/app/dashboard/page.tsx
+++ src/app/(app)/app/dashboard/page.tsx
@@ -5,7 +5,17 @@ import PetList from '@/components/pet-list';
 import SearchForm from '@/components/search-form';
 import Stats from '@/components/stats';
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+  const response = await fetch(
+    'https://bytegrad.com/course-assets/projects/petsoft/api/pets'
+  );
+  if (!response.ok) {
+    throw new Error('Failed to fetch pets data');
+  }
+  const petsData = await response.json();
+  if (!petsData || !Array.isArray(petsData)) {
+    throw new Error('Invalid pets data format');
+  }
   return (
     <main>
       <div className='flex justify-between items-center text-white py-8'>
@@ -21,7 +31,7 @@ export default function DashboardPage() {
         </div>
         <div className='row-start-2 md:row-span-full col-start-1 col-span-1'>
           <ContentBlock>
-            <PetList />
+            <PetList pets={petsData} />
           </ContentBlock>
         </div>
         <div className='md:row-start-1 md:row-span-full md:col-start-2 md:col-span-full'>
--- src/components/pet-list.tsx
+++ src/components/pet-list.tsx
@@ -1,27 +1,28 @@
 import Image from 'next/image';
 
-export default function PetList() {
+export default function PetList({ pets }) {
   return (
     <ul className='bg-white border-b border-black/10'>
-      <li>
-        <button className='flex items-center gap-3 w-full cursor-pointer px-5 py-2 text-base hover:bg-[#eff1f2] focus:bg-[#eff1f2]  focus:outline-none transition '>
-          <Image
-            src={
-              'https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png'
-            }
-            alt='Pet image'
-            width={45}
-            height={45}
-            className='rounded-full object-cover'
-            loading='lazy'
-            unoptimized
-            fetchPriority='low'
-            priority={false}
-            draggable={false}
-          />
-          <p className='font-semibold'>Benjamin</p>
-        </button>
-      </li>
+      {pets.map((pet) => (
+        <li key={pet.id}>
+          <button className='flex items-center gap-3 w-full cursor-pointer px-5 py-2 text-base hover:bg-[#eff1f2] focus:bg-[#eff1f2]  focus:outline-none transition '>
+            <Image
+              src={pet.imageUrl}
+              alt='Pet image'
+              width={45}
+              height={45}
+              className='rounded-full object-cover h-[45px] w-[45px] shrink-0'
+              loading='lazy'
+              unoptimized
+              fetchPriority='low'
+              priority={false}
+              draggable={false}
+            />
+
+            <p className='font-semibold'>{pet.name}</p>
+          </button>
+        </li>
+      ))}
     </ul>
   );
 }
