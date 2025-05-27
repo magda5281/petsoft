a5c694b55514167dac1023fd799fc683a875f5ed
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-05-27 12:11:45 +0100

    Style PetList

--- src/components/pet-list.tsx
+++ src/components/pet-list.tsx
@@ -1,11 +1,26 @@
+import Image from 'next/image';
+
 export default function PetList() {
   return (
     <ul className='bg-white border-b border-black/10'>
       <li>
-        <button>Benjamin</button>
-      </li>
-      <li>
-        <button>Alice</button>
+        <button className='flex items-center gap-3 w-full cursor-pointer px-5 py-2 text-base hover:bg-[#eff1f2] focus:bg-[#eff1f2]  focus:outline-none transition '>
+          <Image
+            src={
+              'https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png'
+            }
+            alt='Pet image'
+            width={45}
+            height={45}
+            className='rounded-full object-cover'
+            loading='lazy'
+            unoptimized
+            fetchPriority='low'
+            priority={false}
+            draggable={false}
+          />
+          <p className='font-semibold'>Benjamin</p>
+        </button>
       </li>
     </ul>
   );
