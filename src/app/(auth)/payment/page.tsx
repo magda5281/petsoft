b11a404c2a12e4ff08b28140fef58fab8d2281a5
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-06-04 16:37:06 +0100

    Payment page toast notification refactor

--- src/app/(auth)/payment/page.tsx
+++ src/app/(auth)/payment/page.tsx
@@ -3,44 +3,38 @@ import { createCheckoutSession } from '@/actions/actions';
 import H1 from '@/components/h1';
 import { Button } from '@/components/ui/button';
 import { useSearchParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useTransition } from 'react';
 import { toast } from 'sonner';
 
-export default function PaymentPage() {
-  const searchParams = useSearchParams();
-  console.log('searchParams', searchParams);
-  const hasSuccess = searchParams?.get('success');
-  const hasCancelled = searchParams?.get('cancelled');
-
-  const [status, setStatus] = useState<'success' | 'cancelled' | ''>('');
+export default function PaymentPage({ searchParams }) {
+  const [isPending, startTransition] = useTransition();
+  //TODO: improve toast implementation
+  const [status, setStatus] = useState(false);
   useEffect(() => {
-    if (hasSuccess) {
-      setStatus('success');
-    }
-    if (hasCancelled) {
-      setStatus('cancelled');
-    }
-    if (!hasSuccess && !hasCancelled) {
-      setStatus('');
+    if (searchParams.success || searchParams.cancelled) {
+      setStatus(true);
     }
   }, [searchParams]);
 
-  if (status === 'success') {
+  if (searchParams.success) {
     toast.success(
       'Payment successful! You now have lifetime access to PetSoft.'
     );
-  } else if (status === 'cancelled') {
+  } else if (searchParams.cancelled) {
     toast.error('Payment cancelled! You can try again.');
   }
 
   return (
     <main className='flex flex-col items-center justify-center space-y-10 mt-4'>
       <H1>PetSoft access requires payment</H1>
-      {!(searchParams && hasSuccess) && (
+      {!(searchParams && searchParams.success) && (
         <Button
           onClick={async () => {
-            await createCheckoutSession();
+            startTransition(async () => {
+              await createCheckoutSession();
+            });
           }}
+          disabled={isPending}
         >
           Buy lifetime access for £299
         </Button>
