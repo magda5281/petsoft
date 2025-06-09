36660953b29f269b297217cfbaf4c95f19038976
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-06-09 12:19:22 +0100

    Add verification for Stripe webhook

--- src/app/(app)/api/stripe/route.ts
+++ src/app/(app)/api/stripe/route.ts
@@ -1,8 +1,33 @@
 import prisma from '@/lib/db';
+import stripe from '@/lib/stripe';
 
 export async function POST(request: Request) {
-  const data = await request.json();
+  const body = await request.text();
+
+  const signature = request.headers.get('stripe-signature');
+  if (!signature) {
+    console.log('Missing Stripe signature header');
+    return Response.json(
+      { error: 'Missing Stripe signature header' },
+      { status: 400 }
+    );
+  }
+
+  if (!process.env.STRIPE_WEBHOOK_SECRET) {
+    throw new Error('Missing env var STRIPE_WEBHOOK_SECRET');
+  }
   //TODO: verify the webhook came from stripe
+  let event;
+  try {
+    event = stripe.webhooks.constructEvent(
+      body,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET
+    );
+  } catch (error) {
+    console.log('Error verifying Stripe webhook:', error);
+    return Response.json(null, { status: 400 });
+  }
 
   //you can use stripe's library to verify the signature
   // const signature = request.headers.get('stripe-signature');
